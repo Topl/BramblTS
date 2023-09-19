a55b15dc30b0eaf2a0b23701fdf7a39969437e97
rayedsikder <rayedsikder33@gmail.com>
2023-09-19 15:00:21 +0600

    Add Either as Type to implement basic functional logic types

---
+++ src/common/either.ts
@@ -0,0 +1,70 @@
+interface Option<T> {
+    isDefined: boolean;
+    getOrElse(defaultValue: T): T;
+    getOrThrow(exception?: Error): T;
+    forEach(f: (t: T) => void): void;
+    map<U>(f: (t: T) => U): Option<U>;
+    flatMap<U>(f: (t: T) => Option<U>): Option<U>;
+    fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U;
+  }
+  
+  class Some<T> implements Option<T> {
+    constructor(public readonly value: T) { }
+  
+    get isDefined(): boolean {
+      return true;
+    }
+  
+    getOrElse(defaultValue: T): T {
+      return this.value;
+    }
+  
+    getOrThrow(): T {
+      return this.value;
+    }
+  
+    map<U>(f: (t: T) => U): Option<U> {
+      return new Some(f(this.value));
+    }
+  
+    forEach(f: (t: T) => void): void {
+      f(this.value);
+    }
+  
+    flatMap<U>(f: (t: T) => Option<U>): Option<U> {
+      return f(this.value);
+    }
+  
+    fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U {
+      return onDefined(this.value);
+    }
+  }
+  
+  class None<T> implements Option<T> {
+    get isDefined(): boolean {
+      return false;
+    }
+  
+    getOrElse(defaultValue: T): T {
+      return defaultValue;
+    }
+  
+    getOrThrow(exception: Error = new Error("Value is not defined.")): T {
+      throw exception;
+    }
+  
+    map<U>(_f: (t: T) => U): Option<U> {
+      return new None<U>();
+    }
+  
+    forEach(_f: (t: T) => void): void { }
+  
+    flatMap<U>(_f: (t: T) => Option<U>): Option<U> {
+      return new None<U>();
+    }
+  
+    fold<U>(_onDefined: (t: T) => U, onUndefined: () => U): U {
+      return onUndefined();
+    }
+  }
+  
