a6cb4693ba3e533fa0f998862ac4bd5c8f62d527
rayedsikder <rayedsikder33@gmail.com>
2023-09-20 16:00:05 +0600

    Export classes and types

--- src/common/either.ts
+++ src/common/either.ts
@@ -1,4 +1,4 @@
-interface Option<T> {
+export interface Option<T> {
     isDefined: boolean;
     getOrElse(defaultValue: T): T;
     getOrThrow(exception?: Error): T;
@@ -6,82 +6,82 @@ interface Option<T> {
     map<U>(f: (t: T) => U): Option<U>;
     flatMap<U>(f: (t: T) => Option<U>): Option<U>;
     fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U;
-  }
-  
-  class Some<T> implements Option<T> {
-    constructor(public readonly value: T) { }
-  
+}
+
+export class Some<T> implements Option<T> {
+    constructor(public readonly value: T) {}
+
     get isDefined(): boolean {
-      return true;
+        return true;
     }
-  
+
     getOrElse(defaultValue: T): T {
-      return this.value;
+        return this.value;
     }
-  
+
     getOrThrow(): T {
-      return this.value;
+        return this.value;
     }
-  
+
     map<U>(f: (t: T) => U): Option<U> {
-      return new Some(f(this.value));
+        return new Some(f(this.value));
     }
-  
+
     forEach(f: (t: T) => void): void {
-      f(this.value);
+        f(this.value);
     }
-  
+
     flatMap<U>(f: (t: T) => Option<U>): Option<U> {
-      return f(this.value);
+        return f(this.value);
     }
-  
+
     fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U {
-      return onDefined(this.value);
+        return onDefined(this.value);
     }
-  }
-  
-  class None<T> implements Option<T> {
+}
+
+export class None<T> implements Option<T> {
     get isDefined(): boolean {
-      return false;
+        return false;
     }
-  
+
     getOrElse(defaultValue: T): T {
-      return defaultValue;
+        return defaultValue;
     }
-  
+
     getOrThrow(exception: Error = new Error("Value is not defined.")): T {
-      throw exception;
+        throw exception;
     }
-  
+
     map<U>(_f: (t: T) => U): Option<U> {
-      return new None<U>();
+        return new None<U>();
     }
-  
-    forEach(_f: (t: T) => void): void { }
-  
+
+    forEach(_f: (t: T) => void): void {}
+
     flatMap<U>(_f: (t: T) => Option<U>): Option<U> {
-      return new None<U>();
+        return new None<U>();
     }
-  
+
     fold<U>(_onDefined: (t: T) => U, onUndefined: () => U): U {
-      return onUndefined();
+        return onUndefined();
     }
-  }
-  
-  type Either<L, R> = { kind: "Left", value: L } | { kind: "Right", value: R };
-  
-  class EitherException extends Error {
+}
+
+export type Either<L, R> = { kind: "Left", value: L } | { kind: "Right", value: R };
+
+export class EitherException extends Error {
     constructor(public readonly message: string) {
-      super(message);
+        super(message);
     }
-  
+
     static rightIsUndefined() {
-      return new EitherException("Right value is undefined!");
+        return new EitherException("Right value is undefined!");
     }
-  
+
     toString(): string {
-      return `EitherException{message: ${this.message}}`;
+        return `EitherException{message: ${this.message}}`;
     }
-  }
-  
-  class Unit { }  
+}
+
+export class Unit {}
