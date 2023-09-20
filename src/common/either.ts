1ab3bc8d6d44760ed5aec441c5016fdbb32731df
rayedsikder <rayedsikder33@gmail.com>
2023-09-20 16:08:43 +0600

    Add classes Digest 32 and Digest 64

--- src/common/either.ts
+++ src/common/either.ts
@@ -1,87 +1,87 @@
 export interface Option<T> {
-    isDefined: boolean;
-    getOrElse(defaultValue: T): T;
-    getOrThrow(exception?: Error): T;
-    forEach(f: (t: T) => void): void;
-    map<U>(f: (t: T) => U): Option<U>;
-    flatMap<U>(f: (t: T) => Option<U>): Option<U>;
-    fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U;
+  isDefined: boolean;
+  getOrElse(defaultValue: T): T;
+  getOrThrow(exception?: Error): T;
+  forEach(f: (t: T) => void): void;
+  map<U>(f: (t: T) => U): Option<U>;
+  flatMap<U>(f: (t: T) => Option<U>): Option<U>;
+  fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U;
 }
 
 export class Some<T> implements Option<T> {
-    constructor(public readonly value: T) {}
+  constructor(public readonly value: T) {}
 
-    get isDefined(): boolean {
-        return true;
-    }
+  get isDefined(): boolean {
+    return true;
+  }
 
-    getOrElse(defaultValue: T): T {
-        return this.value;
-    }
+  getOrElse(defaultValue: T): T {
+    return this.value;
+  }
 
-    getOrThrow(): T {
-        return this.value;
-    }
+  getOrThrow(): T {
+    return this.value;
+  }
 
-    map<U>(f: (t: T) => U): Option<U> {
-        return new Some(f(this.value));
-    }
+  map<U>(f: (t: T) => U): Option<U> {
+    return new Some(f(this.value));
+  }
 
-    forEach(f: (t: T) => void): void {
-        f(this.value);
-    }
+  forEach(f: (t: T) => void): void {
+    f(this.value);
+  }
 
-    flatMap<U>(f: (t: T) => Option<U>): Option<U> {
-        return f(this.value);
-    }
+  flatMap<U>(f: (t: T) => Option<U>): Option<U> {
+    return f(this.value);
+  }
 
-    fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U {
-        return onDefined(this.value);
-    }
+  fold<U>(onDefined: (t: T) => U, onUndefined: () => U): U {
+    return onDefined(this.value);
+  }
 }
 
 export class None<T> implements Option<T> {
-    get isDefined(): boolean {
-        return false;
-    }
+  get isDefined(): boolean {
+    return false;
+  }
 
-    getOrElse(defaultValue: T): T {
-        return defaultValue;
-    }
+  getOrElse(defaultValue: T): T {
+    return defaultValue;
+  }
 
-    getOrThrow(exception: Error = new Error("Value is not defined.")): T {
-        throw exception;
-    }
+  getOrThrow(exception: Error = new Error('Value is not defined.')): T {
+    throw exception;
+  }
 
-    map<U>(_f: (t: T) => U): Option<U> {
-        return new None<U>();
-    }
+  map<U>(_f: (t: T) => U): Option<U> {
+    return new None<U>();
+  }
 
-    forEach(_f: (t: T) => void): void {}
+  forEach(_f: (t: T) => void): void {}
 
-    flatMap<U>(_f: (t: T) => Option<U>): Option<U> {
-        return new None<U>();
-    }
+  flatMap<U>(_f: (t: T) => Option<U>): Option<U> {
+    return new None<U>();
+  }
 
-    fold<U>(_onDefined: (t: T) => U, onUndefined: () => U): U {
-        return onUndefined();
-    }
+  fold<U>(_onDefined: (t: T) => U, onUndefined: () => U): U {
+    return onUndefined();
+  }
 }
 
-export type Either<L, R> = { kind: "Left", value: L } | { kind: "Right", value: R };
+export type Either<L, R> = { kind: 'Left'; value: L } | { kind: 'Right'; value: R };
 
 export class EitherException extends Error {
-    constructor(public readonly message: string) {
-        super(message);
-    }
+  constructor(public readonly message: string) {
+    super(message);
+  }
 
-    static rightIsUndefined() {
-        return new EitherException("Right value is undefined!");
-    }
+  static rightIsUndefined() {
+    return new EitherException('Right value is undefined!');
+  }
 
-    toString(): string {
-        return `EitherException{message: ${this.message}}`;
-    }
+  toString(): string {
+    return `EitherException{message: ${this.message}}`;
+  }
 }
 
 export class Unit {}
--- src/cypto/hash/digest/digest.ts
+++ src/cypto/hash/digest/digest.ts
@@ -1,19 +1,44 @@
-import { Either, EitherException } from "@/common/either.js";
+import { Either, EitherException } from '@/common/either.js';
 
 class Digest {
-  constructor(public readonly bytes: Uint8Array) { }
+  constructor(public readonly bytes: Uint8Array) {}
 
   static empty(): Digest {
     return new Digest(new Uint8Array(0));
   }
 
   equals(other: Digest): boolean {
-    return this.bytes.length === other.bytes.length && 
-           this.bytes.every((value, index) => value === other.bytes[index]);
+    return this.bytes.length === other.bytes.length && this.bytes.every((value, index) => value === other.bytes[index]);
   }
 
   getHashCode(): number {
     return this.bytes.reduce((acc, byte) => acc + byte, 0);
+  }
+}
+
+class Digest32 {
+  private static readonly size: number = 32;
+
+  private constructor() {}
+
+  static from(bytes: Uint8Array): Either<InvalidDigestFailure, Digest> {
+    if (bytes.length !== this.size) {
+      return { kind: 'Left', value: new InvalidDigestFailure(`Invalid digest size: ${bytes.length}`) };
+    }
+    return { kind: 'Right', value: new Digest(bytes) };
+  }
+}
+
+class Digest64 {
+  private static readonly size: number = 64;
+
+  private constructor() {}
+
+  static from(bytes: Uint8Array): Either<InvalidDigestFailure, Digest> {
+    if (bytes.length !== this.size) {
+      return { kind: 'Left', value: new InvalidDigestFailure(`Invalid digest size: ${bytes.length}`) };
+    }
+    return { kind: 'Right', value: new Digest(bytes) };
   }
 }
 
