029ba65327585574f2fd15d93b7c526028520c06
rayedsikder <rayedsikder33@gmail.com>
2023-09-22 20:25:58 +0600

    Fix file paths and comment unimplemented instances

--- src/cypto/hash/hash.ts
+++ src/cypto/hash/hash.ts
@@ -1,14 +1,13 @@
-// import { Blake2b256, Blake2b512, SHA256, SHA512, Digest } from './path/to/brambl_dart';
+import { Digest } from './digest/digest';
 
-// The type that hash operations use as their input.
 type Message = Uint8Array;
 
-export { Blake2b256, Blake2b512, SHA256, SHA512 };
+// export { Blake2b256, Blake2b512, SHA256, SHA512 };
 
 abstract class Hash {
   abstract hash(bytes: Uint8Array): Uint8Array;
 
-  abstract hashComplex(options: { prefix?: number, messages: Message[] }): Digest;
+  abstract hashComplex(options: { prefix?: number; messages: Message[] }): Digest;
 
   hashWithPrefix(prefix: number, messages: Message[]): Digest {
     return this.hashComplex({ prefix, messages });
@@ -19,9 +18,9 @@ abstract class Hash {
   }
 }
 
-const blake2b256 = new Blake2b256();
-const blake2b512 = new Blake2b512();
-const sha256 = new SHA256();
-const sha512 = new SHA512();
+// const blake2b256 = new Blake2b256();
+// const blake2b512 = new Blake2b512();
+// const sha256 = new SHA256();
+// const sha512 = new SHA512();
 
-export { blake2b256, blake2b512, sha256, sha512 };
+// export { blake2b256, blake2b512, sha256, sha512 };
