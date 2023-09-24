eede7dc8df6177d6eb5f81492eea843da9391ecb
rayedsikder <rayedsikder33@gmail.com>
2023-09-24 13:15:53 +0600

    Resolve path and exports
    - Refactor blake2b to blake2B to resolve conflict with blakejs classes
    - Fix exports of Blake2b and SHA instances

--- src/cypto/hash/hash.ts
+++ src/cypto/hash/hash.ts
@@ -1,10 +1,12 @@
 import { Digest } from './digest/digest';
+import { Blake2b256, Blake2b512 } from './blake2B';
+import { SHA256, SHA512 } from './sha';
 
-type Message = Uint8Array;
+export type Message = Uint8Array;
 
-// export { Blake2b256, Blake2b512, SHA256, SHA512 };
+export { blake2b256, blake2b512, sha256, sha512 };
 
-abstract class Hash {
+export abstract class Hash {
   abstract hash(bytes: Uint8Array): Uint8Array;
 
   abstract hashComplex(options: { prefix?: number; messages: Message[] }): Digest;
@@ -18,9 +20,7 @@ abstract class Hash {
   }
 }
 
-// const blake2b256 = new Blake2b256();
-// const blake2b512 = new Blake2b512();
-// const sha256 = new SHA256();
-// const sha512 = new SHA512();
-
-// export { blake2b256, blake2b512, sha256, sha512 };
+const blake2b256 = new Blake2b256();
+const blake2b512 = new Blake2b512();
+const sha256 = new SHA256();
+const sha512 = new SHA512();
