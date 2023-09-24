672cb3ab8aae83a2975d03909df00818d61e950a
rayedsikder <rayedsikder33@gmail.com>
2023-09-24 12:50:33 +0600

    SHA implementation and Digest fixes
    - Implement SHA importing crypto library
    - Fix issue with Digest result checks

---
+++ src/cypto/hash/sha.ts
@@ -0,0 +1,70 @@
+import { Digest, Digest32, Digest64 } from './digest/digest';
+import { Hash, Message } from './hash';
+import * as crypto from 'crypto';
+
+abstract class SHA extends Hash {
+  abstract hash(bytes: Uint8Array): Uint8Array;
+  abstract hashComplex(options: { prefix?: number; messages: Message[] }): Digest;
+}
+
+export class SHA256 extends SHA {
+  hash(bytes: Uint8Array): Uint8Array {
+    const hash = crypto.createHash('sha256');
+    return hash.update(bytes).digest();
+  }
+
+  hashComplex(options: { prefix?: number; messages: Message[] }): Digest {
+    const hash = crypto.createHash('sha256');
+    let input: Uint8Array[] = [];
+    if (options.prefix !== undefined) {
+      input.push(new Uint8Array([options.prefix]));
+    }
+    input = input.concat(options.messages);
+
+    let flattened: number[] = [];
+    input.forEach((arr) => {
+      flattened.push(...arr);
+    });
+
+    const result = hash.update(new Uint8Array(flattened)).digest();
+
+    const digestResult = Digest32.from(result);
+
+    if (digestResult.kind === 'Right') {
+      return digestResult.value;
+    } else {
+      throw digestResult.value;
+    }
+  }
+}
+
+export class SHA512 extends SHA {
+  hash(bytes: Uint8Array): Uint8Array {
+    const hash = crypto.createHash('sha512');
+    return hash.update(bytes).digest();
+  }
+
+  hashComplex(options: { prefix?: number; messages: Message[] }): Digest {
+    const hash = crypto.createHash('sha512');
+    let input: Uint8Array[] = [];
+    if (options.prefix !== undefined) {
+      input.push(new Uint8Array([options.prefix]));
+    }
+    input = input.concat(options.messages);
+
+    let flattened: number[] = [];
+    input.forEach((arr) => {
+      flattened.push(...arr);
+    });
+
+    const result = hash.update(new Uint8Array(flattened)).digest();
+
+    const digestResult = Digest64.from(result);
+
+    if (digestResult.kind === 'Right') {
+      return digestResult.value;
+    } else {
+      throw digestResult.value;
+    }
+  }
+}
