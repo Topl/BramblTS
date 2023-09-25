96f701200e9f749edd91cfe860daf04f7374dd91
rayedsikder <rayedsikder33@gmail.com>
2023-09-25 23:57:03 +0600

    Update tsconfig compiler module to commonjs

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ import { compilerOptions } from './tsconfig.json';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
