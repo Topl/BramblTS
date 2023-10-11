c16bf886f7020607d1e099a06ec235aa969e32ea
dev-shahidul <shahidul.islam@selise.ch>
2023-10-11 13:20:26 +0600

    Add ed25519

--- src/crypto/signing/ed25519/ed25519.ts
+++ src/crypto/signing/ed25519/ed25519.ts
@@ -1,17 +1,83 @@
-import * as ed from '@noble/ed25519';
+// import * as ed from '@noble/ed25519';
 
-class Ed25519 {
-  sign(privateKey: Uint8Array, message: Uint8Array) {
-    return ed.sign(message, privateKey);
+// class Ed25519 {
+//   sign(privateKey: Uint8Array, message: Uint8Array) {
+//     return ed.sign(message, privateKey);
+//   }
+
+//   verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array) {
+//     return ed.verify(signature, message, publicKey);
+//   }
+
+//   getVerificationKey(privateKey: Uint8Array) {
+//     return ed.getPublicKey(privateKey);
+//   }
+// }
+
+// export const ed25519 = new Ed25519();
+
+
+import { Ed25519 as EdDSAEd25519 } from 'ed25519';
+
+import { SecretKey, PublicKey, Ed25519Spec, EllipticCurveSignatureScheme } from './ed25519_spec';
+
+class Ed25519 extends EllipticCurveSignatureScheme<SecretKey, PublicKey> {
+  private impl: EdDSAEd25519;
+
+  constructor() {
+    super(Ed25519Spec.seedLength);
+    this.impl = new EdDSAEd25519();
   }
 
-  verify(signature: Uint8Array, message: Uint8Array, publicKey: Uint8Array) {
-    return ed.verify(signature, message, publicKey);
+  sign(privateKey: SecretKey, message: Uint8Array): Uint8Array {
+    const sig = new Uint8Array(Ed25519Spec.signatureLength);
+    this.impl.sign(
+      privateKey.bytes,
+      0,
+      message,
+      0,
+      message.length,
+      sig,
+      0,
+    );
+    return sig;
   }
 
-  getVerificationKey(privateKey: Uint8Array) {
-    return ed.getPublicKey(privateKey);
+  verify(signature: Uint8Array, message: Uint8Array, publicKey: PublicKey): boolean {
+    const sigByteArray = signature;
+    const vkByteArray = publicKey.bytes;
+    const msgByteArray = message;
+
+    return sigByteArray.length === Ed25519Spec.signatureLength &&
+      vkByteArray.length === Ed25519Spec.publicKeyLength &&
+      this.impl.verify(
+        sigByteArray,
+        0,
+        vkByteArray,
+        0,
+        msgByteArray,
+        0,
+        msgByteArray.length,
+      );
+  }
+
+  getVerificationKey(secretKey: SecretKey): PublicKey {
+    const pkBytes = new Uint8Array(Ed25519Spec.publicKeyLength);
+    this.impl.generatePublicKey(
+      secretKey.bytes,
+      0,
+      pkBytes,
+      0,
+    );
+    return new PublicKey(pkBytes);
+  }
+
+  deriveSecretKeyFromSeed(seed: Uint8Array): SecretKey {
+    if (seed.length < Ed25519Spec.seedLength) {
+      throw new Error(`Invalid seed length. Expected: ${Ed25519Spec.seedLength}, Received: ${seed.length}`);
+    }
+    return new SecretKey(seed.slice(0, 32));
   }
 }
 
-export const ed25519 = new Ed25519();
+export default Ed25519;
--- src/crypto/signing/eddsa/ed25519.ts
+++ src/crypto/signing/eddsa/ed25519.ts
@@ -2,7 +2,7 @@ import { SHA512 } from "@/crypto/hash/sha";
 import { DOM2_PREFIX, EC, POINT_BYTES, PREHASH_SIZE, PointAccum, PointExt, SCALAR_BYTES, SCALAR_INTS, SECRET_KEY_SIZE, SIGNATURE_SIZE } from "./ec";
 import { randomBytes } from "crypto";
 
-class Ed25519 extends EC {
+export class Ed25519 extends EC {
   private readonly defaultDigest = new SHA512();
   private readonly random = randomBytes;
 
