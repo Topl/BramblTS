1656f6ae94e7d2c1489fcd678194bc398d7c094c
rayedsikder <rayedsikder33@gmail.com>
2024-01-13 01:02:06 +0600

    Add kdf.ts

---
+++ src/crypto/encryption/kdf/kdf.ts
@@ -0,0 +1,28 @@
+import { SCrypt, SCryptParams } from './scrypt';
+
+/**
+ * Abstract class representing Key Derivation Functions (KDF).
+ */
+export abstract class Kdf {
+    static fromJson(json: { [key: string]: any }): Kdf {
+        const kdfType = json['kdf'];
+        switch (kdfType) {
+            case 'scrypt':
+                const params = SCryptParams.fromJson(json);
+                return new SCrypt(params);
+            default:
+                throw new Error(`Unknown KDF: ${kdfType}`);
+        }
+    }
+
+    abstract get params(): Params;
+    abstract deriveKey(secret: Buffer): Buffer;
+    abstract toJson(): { [key: string]: any };
+}
+
+/**
+ * Abstract class for KDF parameters.
+ */
+export abstract class Params {
+    abstract get kdf(): string;
+}
