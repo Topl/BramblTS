bc092f4996801fe5bb253d7ca1f5c927ee7e7d89
rayedsikder <rayedsikder33@gmail.com>
2024-01-21 20:15:13 +0600

    Update jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,10 @@ import { compilerOptions } from './tsconfig.json';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    'BramblTS/test/crypto/generation/test_vectors/',
+    'BramblTS/test/crypto/signing/test_vectors/',
+  ],
   testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
