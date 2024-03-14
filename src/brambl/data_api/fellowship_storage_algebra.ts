6bc1f3290e148ce0e440c573e6f5bc7fc945e473
Selby Baidjnath <selby@serubii.com>
2024-03-14 02:34:57 -0300

    Create fellowship_storage_algebra.ts

---
+++ src/brambl/data_api/fellowship_storage_algebra.ts
@@ -0,0 +1,31 @@
+import { ChannelCredentials } from '@grpc/grpc-js';
+import { LockAddress, Txo, TxoState, TransactionServiceClient, QueryByLockAddressRequest} from 'topl_common';
+
+/**
+ * Defines a Genus Query API for interacting with a Genus node.
+ */
+export interface GenusQueryAlgebra {
+
+  /**
+   * Query and retrieve a set of UTXOs encumbered by the given LockAddress.
+   * @param fromAddress The lock address to query the unspent UTXOs by.
+   * @param txoState The state of the UTXOs to query. By default, only unspent UTXOs are returned.
+   * @return A Promise that resolves to an array of UTXOs.
+   */
+  queryUtxo(fromAddress: LockAddress, txoState?: TxoState): Promise<Txo[]>;
+}
+
+export class GenusQueryAlgebraImpl implements GenusQueryAlgebra {
+  private client: TransactionServiceClient;
+
+  constructor(address: string, credentials: ChannelCredentials, options: object) {
+    this.client = new TransactionServiceClient(address, credentials, options);
+  }
+
+  async queryUtxo(fromAddress: LockAddress, txoState: TxoState = TxoState.UNSPENT): Promise<Txo[]> {
+    const response = await this.client.getTxosByLockAddress(
+      new QueryByLockAddressRequest({ address: fromAddress, state: txoState })
+    );
+    return response.txos;
+  }
+}
