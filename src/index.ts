2cacf4c1fa09c3a33a206eeff87be2b1b640e7b2
Selby Baidjnath <selby@serubii.com>
2024-06-04 11:45:32 -0300

    build fixes and index export

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+/// Brambl
+
+//- Builders
+export * from '@/brambl/builders/builder_error.js';
+export * from '@/brambl/builders/locks/lock_template.js';
+export * from '@/brambl/builders/locks/proposition_template.js';
+export * from '@/brambl/builders/transaction_builder_api.js';
+
+//- Codecs
+export * from '@/brambl/codecs/address_codec.js';
+
+//- common
+export * from '@/brambl/common/contains_evidence.js';
+export * from '@/brambl/common/contains_immutable.js';
+export * from '@/brambl/common/contains_signable.js';
+export * from '@/brambl/common/tags.js';
+
+//- constants
+export * from '@/brambl/constants/network_constants.js';
+
+//- data_api
+export * from '@/brambl/data_api/data_api.js';
+
+//- syntax
+export * from '@/brambl/syntax/syntax.js';
+
+// utils
+export * from '@/brambl/utils/encoding.js';
+export * from '@/brambl/utils/extensions.js';
+export * from '@/brambl/utils/extensions_exp.js';
+export * from '@/brambl/utils/guard.js';
+export * from '@/brambl/utils/proto_converters.js';
+
+// validations
+export * from '@/brambl/validation/validations.js';
+
+// wallet
+export * from '@/brambl/wallet/credentialler.js';
+export * from '@/brambl/wallet/credentialler_interpreter.js';
+export * from '@/brambl/wallet/wallet_api.js';
+
+// context
+export * from '@/brambl/context.js';
+
+/// crypto
+export * from '@/crypto/crypto.js';
+
+/// common
+export * from '@/common/functional/brambl_fp.js';
+
+/// quivr4s
+export * from '@/quivr4s/quivr.js';
